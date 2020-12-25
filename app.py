5231cffab040c2026d8690aadf8e2ca7e9a834bc
Daniil Tsebro <39821394+statusxt@users.noreply.github.com>
2020-12-25 12:07:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask import render_template
 import subprocess
 import telnetlib
-import urllib2
+import urllib
 
 user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
 headers = { 'User-Agent' : user_agent }
